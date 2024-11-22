01fd94ed74922c563890b4029699118e239b5c6b
Enforsix <christian.gulkoder@gmail.com>
2024-11-22 12:00:07 +0100

    again

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+//submit Knappen
+const submitBtn = document.querySelector ("#sumit-btn");
+
+
+// personalia elementer
+const nameInpot = document.querySelector ("#name");
+const surenameInpot = document.querySelector ("#surename");
+
+
+// Land element
+const contryInput = document.querySelector("#contry");
+
+
+
+function Submit(event) {
+    event.prenentDefault();
+
+    const name = nameInput.value;
+    const surename = surname.value;
+    const contry = countryInput.value;
+
+    console.log(name);
+    console.log(surename);
+    console.log(contry);
+}
+
+submitBtn.addEventListener("click", submit);
